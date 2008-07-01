e495c740974ca4fc61416042782705ee70ffde44
jgangemi <jgangemi>
2008-07-01 01:33:31 +0000

    - return an AtomicScriptType until real impl. done so values display in Variables view

--- plugins/org.eclipse.dltk.python.debug/src/org/eclipse/dltk/python/debug/PythonTypeFactory.java
+++ plugins/org.eclipse.dltk.python.debug/src/org/eclipse/dltk/python/debug/PythonTypeFactory.java
@@ -1,6 +1,6 @@
 package org.eclipse.dltk.python.debug;
 
-import org.eclipse.dltk.debug.core.model.ComplexScriptType;
+import org.eclipse.dltk.debug.core.model.AtomicScriptType;
 import org.eclipse.dltk.debug.core.model.IScriptType;
 import org.eclipse.dltk.debug.core.model.IScriptTypeFactory;
 
@@ -9,7 +9,7 @@ public class PythonTypeFactory implements IScriptTypeFactory
     public IScriptType buildType(String type)
     {
         // TODO: implement the right way :)
-        return new ComplexScriptType(type);
+        return new AtomicScriptType(type);
     }
 
 }
