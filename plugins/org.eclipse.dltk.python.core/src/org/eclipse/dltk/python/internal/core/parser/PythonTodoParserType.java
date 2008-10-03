3c23325ef619d80b8d1840e2b7ce9675f15a785e
apanchenk <apanchenk>
2008-10-03 13:24:51 +0000

    major builder infrastructure refactoring

--- plugins/org.eclipse.dltk.python.core/src/org/eclipse/dltk/python/internal/core/parser/PythonTodoParserType.java
+++ plugins/org.eclipse.dltk.python.core/src/org/eclipse/dltk/python/internal/core/parser/PythonTodoParserType.java
@@ -1,20 +1,15 @@
 package org.eclipse.dltk.python.internal.core.parser;
 
-import org.eclipse.core.runtime.Preferences;
+import org.eclipse.dltk.compiler.task.ITodoTaskPreferences;
+import org.eclipse.dltk.compiler.task.TodoTaskPreferences;
+import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.builder.AbstractTodoTaskBuildParticipantType;
 import org.eclipse.dltk.python.core.PythonPlugin;
 
-public class PythonTodoParserType extends
-		AbstractTodoTaskBuildParticipantType {
+public class PythonTodoParserType extends AbstractTodoTaskBuildParticipantType {
 
-	private static final String ID = "org.eclipse.dltk.python.todo"; //$NON-NLS-1$
-	private static final String NAME = "Python TODO task parser"; //$NON-NLS-1$
-	
-	public PythonTodoParserType() {
-		super(ID, NAME);
-	}
-	
-	protected Preferences getPreferences() {
-		return PythonPlugin.getDefault().getPluginPreferences();
+	protected ITodoTaskPreferences getPreferences(IScriptProject project) {
+		return new TodoTaskPreferences(PythonPlugin.getDefault()
+				.getPluginPreferences());
 	}
 }
