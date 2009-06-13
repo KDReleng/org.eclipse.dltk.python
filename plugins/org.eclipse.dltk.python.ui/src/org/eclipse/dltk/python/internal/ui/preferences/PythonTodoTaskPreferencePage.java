e8d4fe3b0c39f869a39bf7f5979b98d1b6df7cff
apanchenk <apanchenk>
2009-06-13 05:36:43 +0000

    introduce TodoTaskOptionsBlock class to simplify usage of the abstract class

--- plugins/org.eclipse.dltk.python.ui/src/org/eclipse/dltk/python/internal/ui/preferences/PythonTodoTaskPreferencePage.java
+++ plugins/org.eclipse.dltk.python.ui/src/org/eclipse/dltk/python/internal/ui/preferences/PythonTodoTaskPreferencePage.java
@@ -1,28 +1,17 @@
 package org.eclipse.dltk.python.internal.ui.preferences;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Preferences;
 import org.eclipse.dltk.python.core.PythonNature;
 import org.eclipse.dltk.python.core.PythonPlugin;
 import org.eclipse.dltk.ui.PreferencesAdapter;
 import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPropertyAndPreferencePage;
 import org.eclipse.dltk.ui.preferences.AbstractOptionsBlock;
-import org.eclipse.dltk.ui.preferences.AbstractTodoTaskOptionsBlock;
-import org.eclipse.dltk.ui.preferences.PreferenceKey;
+import org.eclipse.dltk.ui.preferences.TodoTaskOptionsBlock;
 import org.eclipse.dltk.ui.util.IStatusChangeListener;
 import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 
 public class PythonTodoTaskPreferencePage extends
 		AbstractConfigurationBlockPropertyAndPreferencePage {
-
-	static final PreferenceKey CASE_SENSITIVE = AbstractTodoTaskOptionsBlock
-			.createCaseSensitiveKey(PythonPlugin.PLUGIN_ID);
-
-	static final PreferenceKey ENABLED = AbstractTodoTaskOptionsBlock
-			.createEnabledKey(PythonPlugin.PLUGIN_ID);
-
-	static final PreferenceKey TAGS = AbstractTodoTaskOptionsBlock
-			.createTagKey(PythonPlugin.PLUGIN_ID);
 
 	protected String getHelpId() {
 		return null;
@@ -32,29 +21,13 @@ public class PythonTodoTaskPreferencePage extends
 		setDescription(PythonPreferencesMessages.TodoTaskDescription);
 	}
 
-	protected Preferences getPluginPreferences() {
-		return PythonPlugin.getDefault().getPluginPreferences();
-	}
-
 	protected AbstractOptionsBlock createOptionsBlock(
 			IStatusChangeListener newStatusChangedListener, IProject project,
 			IWorkbenchPreferenceContainer container) {
-		return new AbstractTodoTaskOptionsBlock(newStatusChangedListener,
-				project, getPreferenceKeys(), container) {
-			protected PreferenceKey getTags() {
-				return TAGS;
-			}
-
-			protected PreferenceKey getEnabledKey() {
-				return ENABLED;
-			}
-
-			protected PreferenceKey getCaseSensitiveKey() {
-				return CASE_SENSITIVE;
-			}
-		};
+		return new TodoTaskOptionsBlock(newStatusChangedListener, project,
+				container, PythonPlugin.PLUGIN_ID);
 	}
-	
+
 	protected String getNatureId() {
 		return PythonNature.NATURE_ID;
 	}
@@ -69,14 +42,11 @@ public class PythonTodoTaskPreferencePage extends
 	}
 
 	protected String getPreferencePageId() {
-		return "org.eclipse.dltk.python.preferences.todo";
+		return "org.eclipse.dltk.python.preferences.todo"; //$NON-NLS-1$
 	}
 
 	protected String getPropertyPageId() {
-		return "org.eclipse.dltk.python.propertyPage.todo";
+		return "org.eclipse.dltk.python.propertyPage.todo"; //$NON-NLS-1$
 	}
 
-	protected PreferenceKey[] getPreferenceKeys() {
-		return new PreferenceKey[] { TAGS, ENABLED, CASE_SENSITIVE };
-	}
 }
