b1b969ae90145116cd8fd24c01c32fedf8c7b476
apanchenk <apanchenk>
2010-08-09 17:37:52 +0000

    ISelectionEngine refactoring

--- plugins/org.eclipse.dltk.python.core/src/org/eclipse/dltk/python/internal/core/codeassist/PythonSelectionEngine.java
+++ plugins/org.eclipse.dltk.python.core/src/org/eclipse/dltk/python/internal/core/codeassist/PythonSelectionEngine.java
@@ -18,7 +18,6 @@ import org.eclipse.dltk.codeassist.AssistParser;
 import org.eclipse.dltk.codeassist.ScriptSelectionEngine;
 import org.eclipse.dltk.compiler.env.IModuleSource;
 import org.eclipse.dltk.core.DLTKCore;
-import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.dltk.internal.codeassist.select.SelectionNodeFound;
 import org.eclipse.dltk.python.internal.core.parser.PythonParseUtils;
 
@@ -45,8 +44,8 @@ public class PythonSelectionEngine extends ScriptSelectionEngine {
 		// nameEnvironment);
 	}
 
-	public IModelElement[] select(IModuleSource sourceUnit,
-			int selectionSourceStart, int selectionSourceEnd) {
+	public void select(IModuleSource sourceUnit, int selectionSourceStart,
+			int selectionSourceEnd) {
 
 		String source = sourceUnit.getSourceContents();
 		if (DEBUG) {
@@ -60,7 +59,7 @@ public class PythonSelectionEngine extends ScriptSelectionEngine {
 			System.out.println(source);
 		}
 		if (!checkSelection(source, selectionSourceStart, selectionSourceEnd)) {
-			return new IModelElement[0];
+			return;
 		}
 		if (DEBUG) {
 			System.out.print("SELECTION - Checked : \""); //$NON-NLS-1$
@@ -108,9 +107,7 @@ public class PythonSelectionEngine extends ScriptSelectionEngine {
 				e.printStackTrace(System.out);
 			}
 		}
-
-		return (IModelElement[]) selectionElements
-				.toArray(new IModelElement[selectionElements.size()]);
+		reportModelElements(selectionElements);
 	}
 
 	private void select(ASTNode astNode, ASTNode astNodeParent) {
